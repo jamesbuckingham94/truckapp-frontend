01e6d5e6635598f747e31a8920e8d359970fa27e
ivers523 <ivers523@umn.edu>
2020-05-26 17:00:38 -0500

    asdf

--- src/App.js
+++ src/App.js
@@ -8,38 +8,56 @@ import Home from "./views/Home";
 import { List } from "semantic-ui-react";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <nav>
-          <List bulleted horizontal>
-            <List.Item as="a">
-              <Link to="/">Home</Link>
-            </List.Item>
-            <List.Item as="a">
-              <Link to="/consumer">Consumer</Link>
-            </List.Item>
+    return (
 
-            <List.Item as="a">
-              <Link to="/client">Client</Link>
-            </List.Item>
-          </List>
-        </nav>
+        <
+        Router >
+        <
+        div className = "App" >
+        <
+        nav >
+        <
+        List bulleted horizontal >
+        <
+        List.Item as = "a" >
+        <
+        Link to = "/" > Home < /Link> <
+        /List.Item> <
+        List.Item as = "a" >
+        <
+        Link to = "/consumer" > Consumer < /Link> <
+        /List.Item>
 
-        <Switch>
-          <Route path="/client">
-            <Client />
-          </Route>
-          <Route path="/consumer">
-            <Consumer />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+        <
+        List.Item as = "a" >
+        <
+        Link to = "/client" > Client < /Link> <
+        /List.Item> <
+        /List> <
+        /nav>
+
+        <
+        Switch >
+        <
+        Route path = "/client" >
+        <
+        Client / >
+        <
+        /Route> <
+        Route path = "/consumer" >
+        <
+        Consumer / >
+        <
+        /Route> <
+        Route path = "/" >
+        <
+        Home / >
+        <
+        /Route> <
+        /Switch> <
+        /div> <
+        /Router>
+    );
 }
 
 export default App;
