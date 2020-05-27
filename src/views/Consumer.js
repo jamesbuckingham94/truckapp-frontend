a2a3dcebe7e0a125db71c9003661f3d2313f113b
Michael Iverson <ivers523@umn.edu>
2020-05-27 09:57:37 -0500

    created mockup card w/ nested list for trucks

--- src/views/Consumer.js
+++ src/views/Consumer.js
@@ -41,9 +41,40 @@ const Consumer = () => {
       <h2>Consumer</h2>
       <p>This is where we can see location, and food</p>
       <Image style={{marginBottom: '0'}} src="https://picsum.photos/700/250" wrapped ui={false} />
-    <Card style={{marginTop: '0', align: 'center'}}>
-        hi
+      <Card.Group>
+    <Card>
+      <Card.Content>
+        <Card.Header>Almond</Card.Header>
+        <Card.Description>
+        Doc E's BBQ is well renowned throughout Minneapolis, MN, servin' up the heartiest BBQ meats on the streets. It's a food trailer that's perfected that low and slow style, making for meats so juicy and tender, your inner carnivore will come out to play.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content>
+      <List>
+    <List.Item>
+      <List.Icon name='users' />
+      <List.Content>Almond</List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='marker' />
+      <List.Content>Minneapolis, MN</List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='facebook square' />
+      <List.Content>
+        <a href='http://www.facebook.com'>Facebook</a>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='instagram' />
+      <List.Content>
+        <a href='http://www.instagram.com'>Instagram</a>
+      </List.Content>
+    </List.Item>
+  </List>
+      </Card.Content>
     </Card>
+    </Card.Group>
         {/* {trucks
           .filter((truck) => truck.isActive)
           .map((truck) => (
